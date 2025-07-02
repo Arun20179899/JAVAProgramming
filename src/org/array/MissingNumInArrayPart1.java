3977d4bdae3802eb76d5192e95ca2f23a9399ce1
Arun Kumar A S <arun1998.ag12@gmail.com>
2025-07-02 08:01:59 -0700

    Commited New Files

--- src/org/array/MissingNumInArrayPart1.java
+++ src/org/array/MissingNumInArrayPart1.java
@@ -1,6 +1,6 @@
 package org.array;
 
-public class MissingNumInArray {
+public class MissingNumInArrayPart1 {
 
 	public static void main(String[] args) {
 		int data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11 };
